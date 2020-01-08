3f18d426a634371419e321cabf8c8dbf070e6a2f
AdreamCZ <aramde@seznam.cz>
2020-01-08 12:55:44 +0100

    init

--- src/main.c
+++ src/main.c
@@ -6,10 +6,48 @@
 
 int main(void)
 {
-    DDRB = 1 << PB5;
+    
+    PORTB |= (1<<PB7); //Pull up rez na PB7
+    DDRD |= (1 << PD0); 
+
     while (1) {
-        PORTB ^= (1 << PB5);
-        _delay_ms(250);
+        if(!(PINB & (1<<7))){
+            for(int y = 0;y < 7;y++){
+                for(int i =0;i<250;i++){
+                    PORTD ^= (1<<PD0);
+                    switch(y){
+                        case 0:
+                            _delay_ms(1.5);
+                            break;
+                        case 1:
+                            _delay_ms(1.5);
+                            break;
+                        case 2:
+                            _delay_ms(1.5);
+                            break;
+                        case 3:
+                            _delay_ms(3);
+                            break;
+                        case 4:
+                            _delay_ms(1);
+                            break;
+                         case 5:
+                            _delay_ms(5);
+                            break;
+                         case 6:
+                            _delay_ms(5);
+                            break;
+                        
+                    }
+                }
+            if(y%2==0){
+                _delay_ms(50);
+            }else{
+                _delay_ms(150);
+            }
+        }
+        
+        }
     }
 
     return 0;
